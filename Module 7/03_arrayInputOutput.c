2f0e29553645299e27f7a54944021ac28c04d2f5
Fakhrul Islam <fakrulislam0085@gmail.com>
2026-02-14 00:09:29 +0100

    Add Module 7: Array declaration,input,output,reverse of array

---
+++ Module 7/03_arrayInputOutput.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int ar[5];
+   
+    // for(int i=1; i<5; i++)
+    // {
+    //     printf("ar[%d]\n",i);
+    // }
+
+    for(int i=0; i<5; i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(int i=0; i<5; i++)
+    {
+        printf("%d\n",ar[i]);
+    }
+
+    return 0;
+}
---
+++ Module 7/04_arrayInitialization.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int ar[5]={10,23,45,6,07};
+    //int ar[5]={0};
+    
+    for(int i=0; i<5; i++)
+    {
+        printf("%d\n",ar[i]);
+    }
+    return 0;
+}
